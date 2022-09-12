51c76bc6a9bfd07dcca206266a6505bc6478fe9f
Pluriscient <pluriscient@gmail.com>
2022-09-12 14:33:17 +0200

    dotfiles as of 12-08. Added vim

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,19 @@
 if status is-interactive
     atuin init fish | source
     starship init fish | source
+    zoxide init fish | source
+    fish_config theme choose "Dracula"
     source $HOME/.profile
+    if ! which code &> /dev/null
+            
+        eval (zellij setup --generate-auto-start fish | string collect)
+    end        
     # Commands to run in interactive sessions can go here
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/pluriscient/tools/conda/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
+conda deactivate
